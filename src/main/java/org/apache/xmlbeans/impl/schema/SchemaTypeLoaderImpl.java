bcafd21e24e660b77f20f14b816dc44d24fc4431
PJ Fanning <pjfanning@users.noreply.github.com>
2026-06-06 15:35:22 +0100

    Revert "[XMLBeans-661] pass the classloader in getTypeSystemOnClasspath"
    This reverts commit bf0b1412fb94d1c8a80f6f16d128076e9efd324f.

--- src/main/java/org/apache/xmlbeans/impl/schema/SchemaTypeLoaderImpl.java
+++ src/main/java/org/apache/xmlbeans/impl/schema/SchemaTypeLoaderImpl.java
@@ -314,8 +314,7 @@ public class SchemaTypeLoaderImpl extends SchemaTypeLoaderBase {
     }
 
     SchemaTypeSystemImpl getTypeSystemOnClasspath(String name) {
-        return _classpathTypeSystems.computeIfAbsent(name,
-                n -> new SchemaTypeSystemImpl(_resourceLoader, _classLoader, n, this));
+        return _classpathTypeSystems.computeIfAbsent(name, n -> new SchemaTypeSystemImpl(_resourceLoader, n, this));
     }
 
     SchemaTypeSystemImpl getTypeSystemOnClassloader(String name) {
